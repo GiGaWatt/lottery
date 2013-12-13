0440b8ac16f302371d8bf18c58ae00e9cc00f0ed
Jonathan L <jonathanwork856@hotmail.com>
2013-12-13 14:19:38 -0800

    Initial Commit

---
+++ lottery/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  lottery
+//
+//  Created by Jonathan Lopez on 12/13/13.
+//  Copyright (c) 2013 Zeez2weB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
